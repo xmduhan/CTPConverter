bf2ed15c8de41fcaa7f623eba85653e722531c9c
xmduhan <xmduhan@gmail.com>
2015-05-07 01:13:49 +0800

    完成代码生成框架

--- draft.py
+++ draft.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 #%% 代码实线路径
-1、先讲相关的函数列在列在ctpapi.cpp和ctpspi.cpp中。
-2、在converter中增加登录和退出的代码。
-3、监听代码框架编写
-4、生成zmq套接字连接代码。
-5、json的处理（含打包和拆包）
+#1、先将相关的函数列在列在ctpapi.cpp和ctpspi.cpp中。
+#2、在converter中增加登录和退出的代码。
+#3、监听代码框架编写
+#4、生成zmq套接字连接代码。
+#5、json的处理（含打包和拆包）
 
 #%% 修改到项目目录
 import os 
@@ -13,6 +13,7 @@ os.chdir('/home/duhan/github/CTPConverter')
 import cppheader
 from pandas import DataFrame
 import pandas as pd
+from pandas.io.excel import ExcelWriter
 #%% 获取ctp所有的方法名称
 # 获取CThostFtdcTraderSpi的所有方法（以OnRsp,OnRtn,OnErrRtn开头)
 # 获取CThostFtdcTraderApi的所有方法（以Req开头）
@@ -25,7 +26,7 @@ ApiClass = cppheader.getClass(TraderApi_h,'CThostFtdcTraderApi')
 #%% 生成各类相关函数集合
 apiMethodDict = cppheader.getClassMethods(ApiClass,'public')
 spiMethodDict = cppheader.getClassMethods(SpiClass,'public')
-#set(SpiMethods.keys()).intersection(ApiMethods.keys())   # 确认两
+#set(SpiMethods.keys()).intersection(ApiMethods.keys())   # API类和SPI类中没有重名的函数
 methodDict = {}
 methodDict.update(apiMethodDict)
 methodDict.update(spiMethodDict)
@@ -33,13 +34,15 @@ reqMethodDict = {name:apiMethodDict[name] for name in apiMethodDict.keys() if na
 onRspMethodDict = {name:spiMethodDict[name] for name in spiMethodDict.keys() if name.startswith('OnRsp')}
 onRtnMethodDict = {name:spiMethodDict[name] for name in spiMethodDict.keys() if name.startswith('OnRtn')}
 onErrRtnMethodDict = {name:spiMethodDict[name] for name in spiMethodDict.keys() if name.startswith('OnErrRtn')}
-#%%
+#%% 查看所有函数的数量
 print 'len(reqMethodDict.keys())=',len(reqMethodDict.keys())
 print 'len(onRspMethodDict.keys())=',len(onRspMethodDict.keys())
 print 'len(onRtnMethodDict.keys())=',len(onRtnMethodDict.keys())
 print 'len(onErrRtnMethodDict.keys())=',len(onErrRtnMethodDict.keys())
+# OnRsp之所以比Req多一个,因为有一个OnRspError
+# 这个函数的处理方法是否要跟其他OnRsp函数一样，需要进一步确认
 
-#%%
+#%% 确认OnRsp比Req多一个的原因
 d1 = {
 'request':reqMethodDict.keys(),
 'apiname':[ name[3:] for name in reqMethodDict.keys()]
@@ -50,11 +53,37 @@ d2 = {
 'response':onRspMethodDict.keys(),
 'apiname':[ name[5:] for name in onRspMethodDict.keys()]
 }
-df2 = DataFrame(d1)
+df2 = DataFrame(d2)
 
-df = pd.concat({'api':df1,'spi':df2},axis=1)
+df = df1.merge(df2,on='apiname',how='outer')
 
+writer = ExcelWriter('/tmp/output.xls')
+df.to_excel(writer)
+writer.save()
 # 导出excel查看
+
+#%%
+# 根据一个函数名获取函数的定义
+method = methodDict['OnRspQryOrder']
+print method['doxygen']
+print method['name'],'(',
+parameters = cppheader.getMethodParameters(method)
+for p in parameters:
+    print p['type'],p['name']+',',
+print ')'
+
+#%% 根据类型找定义
+
+
+
+#%% 遍历模板目录中的所有模板文件
+import os
+templates = [ i for i in os.listdir('template') if i.endswith('cpp.tpl') ]
+
+
+for template in templates:
+    print '.'.join(template.split('.')[:-1])
+
 
 #%% API 封装的基本结构
 ### 将一个json参数拆包，填如API的参数结构中，调用API。
---
+++ generate.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+import cppheader
+from pandas import DataFrame
+
+# 模板目录
+templateDir = 'template'
+
+def loadCtpHeaderData():
+    '''
+    读取提供模板生态代码的ctp api函数信息
+    '''
+    # c++头文件的定义信息
+    TraderApi_h = cppheader.getCppHeader('include/ThostFtdcTraderApi.h',['TRADER_API_EXPORT'])
+    ApiStruct_h = cppheader.getCppHeader('include/ThostFtdcUserApiStruct.h')
+    typedefDict = cppheader.getTypedefDict('include/ThostFtdcUserApiDataType.h')
+    enumDict = cppheader.getEnumDict('include/ThostFtdcUserApiDataType.h')
+
+    # 从头文件信息中获取API和SPI的类定义信息
+    ApiClass = cppheader.getClass(TraderApi_h,'CThostFtdcTraderApi')
+    SpiClass = cppheader.getClass(TraderApi_h,'CThostFtdcTraderSpi')
+
+    # 获取各类方法信息,目前分为：req,onRsp,onRtn,onErrRtn
+    apiMethodDict = cppheader.getClassMethods(ApiClass,'public')
+    spiMethodDict = cppheader.getClassMethods(SpiClass,'public')
+    reqMethodDict = {name:apiMethodDict[name] for name in apiMethodDict.keys() if name.startswith('Req')}
+    onRspMethodDict = {name:spiMethodDict[name] for name in spiMethodDict.keys() if name.startswith('OnRsp')}
+    onRtnMethodDict = {name:spiMethodDict[name] for name in spiMethodDict.keys() if name.startswith('OnRtn')}
+    onErrRtnMethodDict = {name:spiMethodDict[name] for name in spiMethodDict.keys() if name.startswith('OnErrRtn')}
+    //TODO:OnRspError这个函数比较特殊没有对应的req方法,这个函数是否可以和其他函数相同的处理方法还需要进一步确认.
+
+    # 合并生成一个API和SPI的函数全集,这样可以方便我们用名称类获取函数信息
+    # API类和SPI类中没有重名的函数,所以可以这么做.
+    # 确认API和SPI没有重复：set(SpiMethods.keys()).intersection(ApiMethods.keys())
+    methodDict = {}
+    methodDict.update(apiMethodDict)
+    methodDict.update(spiMethodDict)
+
+    # 数据打包到字典中
+    data = {
+        'reqMethodDict':reqMethodDict,
+        'onRspMethodDict':onRspMethodDict,
+        'onRtnMethodDict':onRtnMethodDict,
+        'onErrRtnMethodDict':onErrRtnMethodDict,
+    }
+
+    return data
+
+
+def renderTemplate(template):
+    '''
+    将一个模板文件转化对应的cpp文件
+    template 模板名称
+    注意：这里假设所有模板文件的的后缀都是".cpp.tpl"
+    '''
+    # 初始化模板系统目录
+    env = Environment(loader=FileSystemLoader(templateDir))
+    outputFileName = '.'.join(template.split('.')[:-1])
+    with open(outputFileName, 'w') as f :
+        # 模板生成到文件
+        f.write(source.render(**data).encode('utf-8'))
+        # 对生成的代码进行格式化
+        os.system('astyle %s' % outputFileName)
+
+
+def main():
+    '''
+    将数filename.cpp.tpl转化为对应的.cpp文件
+    '''
+    # 读取ctp接口的各类定义信息,用于生成模板
+    data = loadCtpHeaderData()
+
+    # 读取所有模板列表
+    templates = [ i for i in os.listdir(templateDir) if i.endswith('cpp.tpl') ]
+
+    # 遍历所有cpp.tpl模板文件,并生成.cpp文件
+    for template in templates:
+        renderTemplate(template)
+
+
+
+if __name__ == '__main__':
+    main()
