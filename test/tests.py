aef84294bd3415d9acc9f2989f0363c34d4facb9
xmduhan <xmduhan@gmail.com>
2015-05-26 11:29:07 +0800

    ...

--- test/tests.py
+++ test/tests.py
@@ -25,7 +25,8 @@ def test_ReqQryTradingAccount_0():
 	测试ReqQryTradingAccount xxx
 	其他的一些说明
 	'''
-	apiName = 'ReqQryTradingAccount'
+	requestApiName = 'ReqQryTradingAccount'
+	responseApiName = 'OnRspQryTradingAccount'
 	timeout = 1000
 
 	# 初始化变量
@@ -40,21 +41,21 @@ def test_ReqQryTradingAccount_0():
 	socket.setsockopt(zmq.LINGER,0)
 
 	# 准备调用接口数据
-	reqInfo = getDefaultReqInfo(apiName)
+	reqInfo = getDefaultReqInfo(requestApiName)
 	data = reqInfo['Parameters']['Data']
 	metaData = {}
 	# TODO 填写对应的请求参数(data)
 
 	requestData = []
 	requestData.append('REQUEST')  # header
-	requestData.append(apiName)  # apiName
+	requestData.append(requestApiName)  # requestApiName
 	requestData.append(json.dumps(reqInfo)) # reqInfo
 	requestData.append(json.dumps(metaData))  # metaData
 
 	# 开始计时
 	startTime = datetime.now()
 	# 向协议转换器发出请求
-	socket.send_multipart(*requestData)
+	socket.send_multipart(requestData)
 	################### 等待服务器的REQUESTID响应 ###################
 	poller = zmq.Poller()
 	poller.register(socket, zmq.POLLIN)
@@ -63,15 +64,15 @@ def test_ReqQryTradingAccount_0():
 
 	# 从request通讯管道读取返回信息
 	requestIdData = {}
-	requestIdData['header'],requestIdData['requestID'],requestIdData['apiName'],\
+	requestIdData['header'],requestIdData['requestID'],requestIdData['requestApiName'],\
 	requestIdData['errorInfo'],requestIdData['metaData'] = socket.recv_multipart()
 
 	# 检查返回的信息是否符合预期
 	assert requestIdData['header'] == 'REQUESTID'
 	assert int(requestIdData['requestID']) > 0
-	assert requestIdData['apiName'] == apiName
+	assert requestIdData['requestApiName'] == requestApiName
 	assert requestIdData['errorInfo'] == ''
-	assert requestIdData['metaData'] == metaData
+	assert requestIdData['metaData'] == json.dumps(metaData)
 
 
 	################### 等待服务器的返回的数据信息 ###################
@@ -82,14 +83,19 @@ def test_ReqQryTradingAccount_0():
 
 	# 从request通讯管道读取返回信息
 	responseData = {}
-	responseData['header'],responseData['requestID'],responseData['apiName'],\
+	responseData['header'],responseData['requestID'],responseData['responseApiName'],\
 	responseData['respInfo'],responseData['metaData'] = socket.recv_multipart()
 
 	# TODO 检查调用是否成功
+	assert responseData['header'] == 'RESPONSE'
+	assert responseData['requestID'] == requestIdData['requestID']
+	assert responseData['responseApiName'] == responseApiName
+	assert responseData['metaData'] == json.dumps(metaData)
+
 	print "respHeader=",responseData['header']
 	print "respRequestID=",responseData['requestID']
-	print "respApiName=",responseData['apiName']
-	print "respRespInfo=",responseData['respInfo']
+	print "respApiName=",responseData['responseApiName']
+	#print "respRespInfo=",responseData['respInfo']
 	print "respMetaData=",responseData['metaData']
 	# TODO 显示关键信息
 
