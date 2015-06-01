cf095fcee4ae718d9a193eeb6cc72fa10049740c
xmduhan <xmduhan@gmail.com>
2015-06-01 11:03:07 +0800

    ...

--- converter.cpp
+++ converter.cpp
@@ -12,7 +12,8 @@ static Configure config;
 
 static char buffer[1024*10];
 
-#define REQUEST_ROUTE_TTL 30
+#define ROUTE_MESSAGE_TTL 60
+
 
 // 返回信息路由表
 struct RouteTableItem {
@@ -131,7 +132,7 @@ int main() {
                         RouteTableItem * pRouteTableItem = new RouteTableItem();
                         pRouteTableItem -> routeKey = requestMessage.routeKey;
                         pRouteTableItem -> metaData = requestMessage.metaData;
-                      	pRouteTableItem -> ttl = REQUEST_ROUTE_TTL;
+                        pRouteTableItem -> ttl = ROUTE_MESSAGE_TTL;
                         routeTable[requestID] = pRouteTableItem;
                     } catch(std::exception & e) {
                         std::cout << "main():异常:" << e.what() << std::endl;
