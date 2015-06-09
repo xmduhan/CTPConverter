7eef31cadf6e27052ed989476216b9c2c0a66980
xmduhan <xmduhan@gmail.com>
2015-06-09 16:28:54 +0800

    增加启动延时处理，和发送测试消息

--- md.cpp
+++ md.cpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <json/json.h>
 
+
 static MdConfigure config;
 static char buffer[1024*10];
 
@@ -17,11 +18,6 @@ int main(int argc,char * argv[]){
         config.loadFromCommandLine(commandOption);
     }
 
-    // 初始化api接口实例
-    CMdWrapper api(&config);
-    api.init();
-
-
     // 初始化zmq环境
     zmq::context_t context(1);
     zmq::socket_t pushback  (context, ZMQ_PULL);
@@ -32,6 +28,12 @@ int main(int argc,char * argv[]){
     publish.bind(config.publishPipe);
 
     std::cout << "main():行情广播地址为:" << config.publishPipe << std::endl;
+    sleep(1);    // 给一定时间让订阅者连接上来，以免错过条消息
+
+
+    // 初始化api接口实例
+    CMdWrapper api(&config);
+    api.init();
 
     // 定义消息变量
     PushbackMessage pushbackMessage;
@@ -46,19 +48,31 @@ int main(int argc,char * argv[]){
 
     std::cout << "main():转发市场报价信息..." << std::endl;
     api.SubscribeMarketData(config.instrumentIDArray,config.instrumentCount);
+
+    // 发送一条空消息供订阅客户端测试通路.
+    // 注意:程序能执行到这里,说明已成功和ctp服务器建立连接.
+    // 所以当客户端收到这条空消息时可以认为连接行情通道建立成功.
+    marketDataMessage.marketDataInfo = "";
+    marketDataMessage.send(publish);
+
     while(1) {
         zmq::pollitem_t pullItems [] = {
             { pushback, 0, ZMQ_POLLIN, 0 }
         };
         zmq::poll (pullItems, 1, timeout);
         if ( pullItems[0].revents & ZMQ_POLLIN) {
+
+            // 从pushback管道读取消息
             pushbackMessage.recv(pushback);
+
+            // 如果消息是行情数据,将其返回客户端
             if (pushbackMessage.apiName == "OnRtnDepthMarketData") {
                 marketDataMessage.marketDataInfo = pushbackMessage.respInfo;
                 marketDataMessage.send(publish);
                 mdCount++;
             }
 
+            // 如果消息是订阅处理结果将其显示出来
             if (pushbackMessage.apiName == "OnRspSubMarketData") {
                 Json::Reader jsonReader;
                 Json::Value respInfo;
